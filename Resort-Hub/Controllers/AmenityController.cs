9d812f4c331ad2dea1e1fb25b39791bba28d26ad
marym <Marymfoda138@gmail.com>
2026-04-18 21:43:23 +0200

    Create CRUD for villas

---
+++ Resort-Hub/Controllers/AmenityController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Resort_Hub.Entities;
+using Resort_Hub.Interfaces;
+
+namespace Resort_Hub.Controllers;
+
+public class AmenityController(IUnitOfWork unitOfWork) : Controller
+{
+    public IActionResult Index()
+    {
+        return View(unitOfWork.Amenities.GetAll());
+    }
+
+    public IActionResult Create() => View(new Amenity());
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(Amenity amenity)
+    {
+        if (!ModelState.IsValid) return View(amenity);
+
+        unitOfWork.Amenities.Add(amenity);
+        await unitOfWork.SaveAsync();
+
+        TempData["Success"] = "Amenity created!";
+        return RedirectToAction(nameof(Index));
+    }
+
+    public IActionResult Edit(int id)
+    {
+        var amenity = unitOfWork.Amenities.GetById(id);
+        if (amenity is null) return NotFound();
+        return View(amenity);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(Amenity amenity)
+    {
+        if (!ModelState.IsValid) return View(amenity);
+
+        unitOfWork.Amenities.Update(amenity);
+        await unitOfWork.SaveAsync();
+
+        TempData["Success"] = "Amenity updated!";
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var amenity = unitOfWork.Amenities.GetById(id);
+        if (amenity is null) return NotFound();
+
+        unitOfWork.Amenities.Delete(amenity);
+        await unitOfWork.SaveAsync();
+
+        TempData["Success"] = "Amenity deleted!";
+        return RedirectToAction(nameof(Index));
+    }
+}
---
+++ Resort-Hub/ViewModels/Villa/VillaFormVM.cs
@@ -0,0 +1,33 @@
+﻿using Resort_Hub.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace Resort_Hub.ViewModels.Villa;
+
+public class VillaFormVM
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Name is required")]
+    public string Name { get; set; } = string.Empty;
+
+    public string? Description { get; set; }
+
+    [Range(1, double.MaxValue, ErrorMessage = "Price must be greater than 0")]
+    public double PricePerNight { get; set; }
+
+    [Range(1, int.MaxValue, ErrorMessage = "Sqft must be greater than 0")]
+    public int Sqft { get; set; }
+
+    [Range(1, int.MaxValue, ErrorMessage = "Capacity must be greater than 0")]
+    public int Capacity { get; set; }
+
+    public bool IsAvilable { get; set; } = true;
+
+    // Amenities
+    public List<int> SelectedAmenityIds { get; set; } = [];
+    public List<Amenity> AvailableAmenities { get; set; } = [];
+
+    // Images
+    public List<IFormFile>? NewImages { get; set; }
+    public List<VillaImage> ExistingImages { get; set; } = [];
+}
